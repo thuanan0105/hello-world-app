0b7d7f7e8a062d183c37f3f6a7b2ac523635131b
anttt <an.tran@dirox.net>
2021-02-23 11:58:10 +0700

    update version for package

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,10 @@
 import { Api } from "./api";
 import { AxiosApi } from "./axiosApi";
 import { BaseApi } from "./baseApi";
-import { FetchApi } from "./fetchApi";
 
 export {
   Api,
   AxiosApi,
   BaseApi,
-  FetchApi
 };
 
